ad66173cc4303b5613e6bd949e463e92d31cc658
Joe Russell <joerussell@joes-air.lan>
2026-02-18 21:32:11 -0800

    Fix: Correct capitalization for App-Firebase import

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App-firebase';
+import App from './App-Firebase';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
